ec5002120366f2b875023749a7a6f9ec63559d35
olathedev <olapademicheal3@gmail.com>
2024-08-09 11:21:22 -0700

    fix in footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = (props: Props) => {
         <footer className='mt-20 bg-black text-white '>
             <div className="container mx-auto px-24 flex flex-col">
                 <div className='flex flex-col gap-4 mt-24 mb-16'>
-                    <h1 className='font-bold text-6xl'>Simplify payments
+                    <h1 className='font-bold text-5xl leading-snug text-white'>Simplify payments <br />
                         and money management</h1>
 
                     <h3 className="text-2xl">Drive revenue, reduce costs, and manage funds with Mollie.</h3>
@@ -23,7 +23,7 @@ const Footer = (props: Props) => {
                     </div>
                 </div>
 
-                <div className='grid grid-cols-5 justify-between text-lg font-semibold text-gray-600 py-10'>
+                <div className='grid grid-cols-5 justify-between text-lg text-gray-500 py-10'>
                     <div className='flex flex-col space-y-3'>
                         <h4 className='text-white'>Mollie</h4>
 
@@ -57,6 +57,23 @@ const Footer = (props: Props) => {
                         <h4 className='text-white'>Location and language</h4>
                         
                     </div>
+                </div>
+
+
+                <div className='flex justify-between text-gray-500 mb-4'>
+                        <div className='flex gap-4 text-sm'>
+                            <div> &copy; 2024 Mollie B.V.</div>
+                            <div>User Agreement</div>
+                            <div>Privacy Statement</div>
+                            <div>Responsible Disclosure</div>
+                            <div>Whistleblowe policy</div>
+                            <div>Impressum</div>
+                            <div>Cookie settings</div>
+                        </div>
+
+                        <div>
+                            
+                        </div>
                 </div>
             </div>
         </footer>
